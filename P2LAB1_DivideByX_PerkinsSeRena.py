2224bddcfb3211d5c50ede0b66d7df96179d73e3
perkinss0404 <78818306+perkinss0404@users.noreply.github.com>
2021-02-24 12:32:28 -0500

    Submission 6
    Submission 6

---
+++ P2LAB1_DivideByX_PerkinsSeRena.py
@@ -0,0 +1,13 @@
+
+user_num = int(input())
+x = int(input())
+
+
+answer1 = user_num // x
+answer2 = answer1 // x
+answer3 = answer2 // x
+
+
+print(answer1,end=' ')
+print(answer2, end=' ')
+print(answer3)
