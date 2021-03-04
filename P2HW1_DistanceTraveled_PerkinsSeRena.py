0916191185c28fecff648bc20a941b85d34c4b1b
perkinss0404 <78818306+perkinss0404@users.noreply.github.com>
2021-03-04 12:49:07 -0500

    Submission 8
    Submission 8

---
+++ P2HW1_DistanceTraveled_PerkinsSeRena.py
@@ -0,0 +1,22 @@
+#Module 4 Homework
+#March 3 2021
+#CTI-110 P2HW1 - Distance Traveled
+#SeRena Perkins
+#Input Section
+#Initial total is zero
+#Input the car speed, save as speed
+#Input the hours traveled, save as time
+#Multiply speed and distance
+#Output quotient
+
+
+print('Enter car\'s speed:')
+speed = float(input())
+print('Enter time traveled:')
+time = float(input())
+print()
+print('Speed entered:', speed)
+print('Time entered:', time)
+print()
+quotient = speed * time
+print('Distance Traveled', quotient, 'miles')
